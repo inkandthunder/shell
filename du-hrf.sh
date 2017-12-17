7ce34613a7092d80836e478ec1e00c7b12990e6b
Scott DeHaan <scottdehaan@gmail.com>
2017-12-17 09:10:44 -0600

    Update du-hrf.sh

--- du-hrf.sh
+++ du-hrf.sh
@@ -4,7 +4,7 @@
 #author          :Scott DeHaan
 #version         :1.0    
 #cron            :none
-#notes			     :echo -e prints G for Gigabytes, M for Megabytes and K for Kilobytes in a line each; 2>/dev/null send stderr to /dev/null; sort -rn sorts in reverse numerical order. Largest first
+#notes           :echo -e prints G for Gigabytes, M for Megabytes and K for Kilobytes in a line each; 2>/dev/null send stderr to /dev/null; sort -rn sorts in reverse numerical order. Largest first
 #bash_version    :GNU bash, version 3.2.25(1)-release (x86_64-redhat-linux-gnu)
 #==============================================================================
 
